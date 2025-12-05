0e1377269afc7dffe5971e4c157b43609f72c224
Joel Rosario <cirquitz@gmail.com>
2025-12-05 22:28:48 +0530

    Cleanup the build files and deprecated class in the application

--- src/main/kotlin/io/specmatic/inventory/config/WebServiceConfig.kt
+++ src/main/kotlin/io/specmatic/inventory/config/WebServiceConfig.kt
@@ -5,13 +5,12 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.ws.config.annotation.EnableWs
-import org.springframework.ws.config.annotation.WsConfigurerAdapter
+import org.springframework.ws.config.annotation.WsConfigurer
 import org.springframework.ws.transport.http.MessageDispatcherServlet
-
 
 @EnableWs
 @Configuration
-class WebServiceConfig : WsConfigurerAdapter() {
+class WebServiceConfig : WsConfigurer {
     @Bean
     fun messageDispatcherServlet(applicationContext: ApplicationContext): ServletRegistrationBean<MessageDispatcherServlet> {
         val servlet = MessageDispatcherServlet()
