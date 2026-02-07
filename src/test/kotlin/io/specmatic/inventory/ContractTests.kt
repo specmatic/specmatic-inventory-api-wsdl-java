f10c7afbe801c3f65c73e7483c666968a935856f
Joel Rosario <cirquitz@gmail.com>
2026-02-07 17:33:41 +0530

    Updated specmatic config to v3

--- src/test/kotlin/io/specmatic/inventory/ContractTests.kt
+++ src/test/kotlin/io/specmatic/inventory/ContractTests.kt
@@ -8,22 +8,4 @@ import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class ContractTests : SpecmaticContractTest {
-
-    companion object {
-        private const val APPLICATION_HOST = "localhost"
-        private const val APPLICATION_PORT = "8095"
-
-        @JvmStatic
-        @BeforeAll
-        fun setUp() {
-            System.setProperty("host", APPLICATION_HOST)
-            System.setProperty("port", APPLICATION_PORT)
-        }
-
-        @JvmStatic
-        @AfterAll
-        fun tearDown() {
-        }
-    }
-}
+class ContractTests : SpecmaticContractTest
